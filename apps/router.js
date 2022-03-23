ba02ad139d51bc1f13a324d9815d4889a2a01aee
uragen <homeliaisonphoto@gmail.com>
2022-03-23 17:42:07 +0900

    Room_220323_v3

--- apps/router.js
+++ apps/router.js
@@ -19,6 +19,80 @@ Router.prototype.rou_get_First = function () {
   return obj;
 }
 
+Router.prototype.rou_post_generalMongo = function () {
+  const instance = this;
+  const { equalJson, fileSystem } = this.mother;
+  let obj = {};
+  obj.link = "/generalMongo";
+  obj.func = async function (req, res) {
+    res.set({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, HEAD",
+      "Access-Control-Allow-Headers": "Content-Type, Accept, X-Requested-With, remember-me",
+    });
+    try {
+      if (req.body.mode === undefined) {
+        throw new Error("must be mode => [ create, read, update, delete ]");
+      }
+      if (req.body.collection === undefined) {
+        throw new Error("must be collection name");
+      }
+      const { mode, collection } = req.body;
+      const db = "miro81";
+      let selfMongo, result;
+      let whereQuery, updateQuery;
+      let rows;
+
+      selfMongo = instance.mongo;
+
+      if (mode === "read") {
+        if (req.body.whereQuery === undefined) {
+          throw new Error("must be whereQuery");
+        }
+        whereQuery = equalJson(req.body.whereQuery);
+        result = await selfMongo.db(db).collection(collection).find(whereQuery).toArray();
+
+      } else if (mode === "update") {
+        if (req.body.whereQuery === undefined || req.body.updateQuery === undefined) {
+          throw new Error("must be whereQuery and updateQuery");
+        }
+        whereQuery = equalJson(req.body.whereQuery);
+        updateQuery = equalJson(req.body.updateQuery);
+        result = await selfMongo.db(db).collection(collection).find(whereQuery).toArray();
+        if (result.length !== 0) {
+          await selfMongo.db(db).collection(collection).updateOne(whereQuery, { $set: updateQuery });
+        }
+        result = { message: "done" };
+
+      } else if (mode === "create") {
+        if (req.body.updateQuery === undefined) {
+          throw new Error("must be updateQuery");
+        }
+        updateQuery = equalJson(req.body.updateQuery);
+        await selfMongo.db(db).collection(collection).insertOne(updateQuery);
+        result = { message: "done" };
+
+      } else if (mode === "delete") {
+        if (req.body.whereQuery === undefined) {
+          throw new Error("must be whereQuery");
+        }
+        whereQuery = equalJson(req.body.whereQuery);
+        await selfMongo.db(db).collection(collection).deleteOne(whereQuery);
+        result = { message: "done" };
+
+      } else {
+        throw new Error("must be mode => [ create, read, update, delete ]");
+      }
+
+      res.send(JSON.stringify(result));
+    } catch (e) {
+      res.send({ message: e.message });
+    }
+  }
+  return obj;
+}
+
 Router.prototype.getAll = function () {
   let result, result_arr;
 
